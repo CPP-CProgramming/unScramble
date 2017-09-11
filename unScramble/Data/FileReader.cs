ab9c0587be325212dd99fb609b62bfd7b308c6df
inchan hwang <neogeo-s@hanmail.net>
2017-09-11 22:36:03 +0900

    2nd commit

---
+++ unScramble/Data/FileReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace unScramble.Data
+{
+    struct MatchedWord
+    {
+        public string ScrambledWord { get; set; }
+        public string Word { get; set; }
+
+        public  string[] Read(string filename)
+        {
+            string[] fileContent;
+            try
+            {
+                fileContent = File.ReadAllLines(filename);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return fileContent;
+        }
+    }
+}
--- unScramble/Program.cs
+++ unScramble/Program.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using unScramble.Data;
+using unScramble.Workers;
 
 namespace unScramble
 {
     class Program
     {
+        private static readonly MatchedWord _fileReader = new MatchedWord();
+        private static readonly WordMatcher _wordMatcher = new WordMatcher();
+
+        private const string wordListFileName = "wordlist.txt";
         static void Main(string[] args)
         {
             bool continueWordUnscramble = true;
             
-
-           
+            
             do
             {
                 Console.WriteLine("Please enter the option - F for file and M for Manual");
@@ -34,8 +39,7 @@ namespace unScramble
                     default:
                         Console.Write("Option was not recognized");
                         break;
-
-                    
+                        
                 }
 
                 var continueWordUnscrambleDecision = string.Empty;
@@ -43,7 +47,9 @@ namespace unScramble
                 {
                     Console.WriteLine("Do you want to continue? Y/N");
                     continueWordUnscrambleDecision = (Console.ReadLine() ?? string.Empty);
-                } while (!continueWordUnscrambleDecision.Equals("Y", StringComparison.OrdinalIgnoreCase) && !continueWordUnscrambleDecision.Equals("N", StringComparison.OrdinalIgnoreCase));
+                } while (
+                !continueWordUnscrambleDecision.Equals("Y", StringComparison.OrdinalIgnoreCase) && 
+                !continueWordUnscrambleDecision.Equals("N", StringComparison.OrdinalIgnoreCase));
 
                 continueWordUnscramble = continueWordUnscrambleDecision.Equals("Y", StringComparison.OrdinalIgnoreCase);
 
@@ -56,7 +62,28 @@ namespace unScramble
         }
         private static void ExecuteScrambleWordsManualEntryScenario()
         {
-            throw new NotImplementedException();
+            var filename = Console.ReadLine() ?? string.Empty;
+            string[] scrambledWords = _fileReader.Read(filename);
+            DisplayMatchedUnscrambledWords(scrambledWords);
+        }
+
+        private static void DisplayMatchedUnscrambledWords(string[] scrambledWords)
+        {
+            string[] wordList = _fileReader.Read(wordListFileName);
+
+            List<MatchedWord> matchedWords = _wordMatcher.Match();
+
+            if (matchedWords.Any())
+            {
+                foreach (var matchedWord in matchedWords)
+                {
+                    Console.WriteLine("Match found for {0}: {1} ", matchedWord.ScrambledWord, matchedWord.Word);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No matches have been found.");
+            }
         }
     }
 }
